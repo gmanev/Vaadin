5f0d508e832046fcb153d36d28198258a087fb06
Georgi Manev <gmanev@gmail.com>
2014-11-10 15:14:59 +0200

    initial

---
+++ vaadin-parent/vaadin-bridge/src/main/java/com/jjinterna/vaadin/vaadinbridge/internal/VaadinBridgeActivator.java
@@ -0,0 +1,13 @@
+package com.jjinterna.vaadin.vaadinbridge.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+
+public class VaadinBridgeActivator implements BundleActivator {
+	public void start(BundleContext context) throws Exception {
+		new HttpServiceTracker(context).open();
+	}
+	public void stop(BundleContext context) throws Exception {
+	}
+}
